ba184db839cfee2a0898f4325caf3293f6ccb4bf
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-22 11:04:37 -0300

    Add InputSelect

---
+++ src/components/ui/InputSelect/index.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { ChangeEvent, ReactElement, useId } from 'react';
+
+interface Props {
+  icon: ReactElement;
+  values: string[];
+  label?: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export const InputSelect = ({ icon: Icon, values, label, onChange }: Props) => {
+  const id = useId();
+
+  return (
+    <>
+      { label && (
+        <label htmlFor={ id } className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          { label }
+        </label>
+      ) }
+      <div className="flex gap-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        { Icon }
+        <select id={ id } className="bg-transparent w-full outline-none" onChange={ onChange }>
+          { values.map((value, i) => (
+            <option key={ i } value={ value }>{ value }</option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+};
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export * from './Button';
 export * from './MainLogo';
 export * from './Header';
 export * from './Spinner';
+export * from './InputSelect';
