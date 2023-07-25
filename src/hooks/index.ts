a0e3c908296c6ad0b7c984fca5988e1b242cb87e
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-24 21:00:32 -0300

    Add useSearchbar

---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useSearchbar } from './useSearchbar';
---
+++ src/hooks/useSearchbar.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useContext, useEffect, useId, useRef } from 'react';
+import { CatalogContext } from '@/contexts/catalog';
+
+const useSearchbar = (handleOpen: Dispatch<SetStateAction<boolean>> | null) => {
+  const { params, changeSearchText, searchBook } = useContext(CatalogContext);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const id = useId();
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    changeSearchText(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchBook(params.filters?.searchText || '');
+    if (handleOpen) {
+      handleOpen(false);
+    }
+  };
+
+  return {
+    id,
+    inputRef,
+    inputValue: params.filters?.searchText || '',
+    handleInputChange,
+    handleSubmit,
+  };
+};
+
+export default useSearchbar;
