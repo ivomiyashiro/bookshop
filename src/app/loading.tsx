e7660fc686bb8f4296da6d4efc0c86a178db637b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-31 17:00:29 -0400

    Add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,21 @@
+import { ProductCardSkeleton } from '@/components/ui';
+
+export default function Loading() {
+  return (
+    <>
+      <div className="grid grid-cols-2 md:flex gap-2 md:gap-4 mb-10 w-full h-[54px] animate-pulse">
+        <div className="flex gap-2 w-full h-full rounded-lg dark:bg-gray-700 bg-gray-200">
+        </div>
+        <div className="flex gap-2 md:gap-4 h-full w-full">
+          <div className="h-full w-full rounded-lg dark:bg-gray-700 bg-gray-200">
+          </div>
+          <div className="h-full w-full rounded-lg dark:bg-gray-700 bg-gray-200">
+          </div>
+        </div>
+      </div>
+      <div className={ `grid ${ 'GRID' !== 'GRID' ? 'grid-cols-2' : 'grid-cols-1' } md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 gap-y-6 overflow-hidden max-w-[1340px] mx-auto` }>
+        { new Array(10).fill(null).map((_val, i) => <ProductCardSkeleton key={ i } /> )  }
+      </div>
+    </>
+  );
+}
